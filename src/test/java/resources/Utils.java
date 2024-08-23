95374e2abaea5afe77d65a0be4cb2185eedcda31
Artem Trifonov <artemis.levitt@gmail.com>
2024-08-23 20:16:49 +0200

    Move baseUrl to global.properties

--- src/test/java/resources/Utils.java
+++ src/test/java/resources/Utils.java
@@ -1,27 +1,24 @@
 package resources;
 
-import io.restassured.RestAssured;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.filter.log.RequestLoggingFilter;
 import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.PrintStream;
+import java.io.*;
+import java.util.Properties;
 
 public class Utils {
 
     RequestSpecification req;
 
-    public RequestSpecification requestSpecification() throws FileNotFoundException {
-        RestAssured.baseURI = "https://rahulshettyacademy.com";
+    public RequestSpecification requestSpecification() throws IOException {
 
         PrintStream log = new PrintStream(new FileOutputStream("logging.txt"));
 
         req = new RequestSpecBuilder()
-            .setBaseUri("https://rahulshettyacademy.com")
+            .setBaseUri(getGlobalValue("baseUrl"))
             .addQueryParam("key", "qaclick123")
             .setContentType(ContentType.JSON)
             .addFilter(RequestLoggingFilter.logRequestTo(log))
@@ -30,5 +27,12 @@ public class Utils {
         ;
 
         return req;
+    }
+
+    public static String getGlobalValue(String key) throws IOException {
+        Properties properties = new Properties();
+        FileInputStream fis = new FileInputStream("src/test/java/resources/global.properties");
+        properties.load(fis);
+        return properties.getProperty(key);
     }
 }
--- src/test/java/stepDefinitions/AddPlaceStepDefinition.java
+++ src/test/java/stepDefinitions/AddPlaceStepDefinition.java
@@ -13,6 +13,7 @@ import resources.TestDataBuild;
 import resources.Utils;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 import static io.restassured.RestAssured.*;
 import static org.junit.Assert.*;
@@ -25,7 +26,7 @@ public class AddPlaceStepDefinition extends Utils {
     TestDataBuild data = new TestDataBuild();
 
     @Given("Add Place Payload")
-    public void add_place_payload() throws FileNotFoundException {
+    public void add_place_payload() throws IOException {
 
         res =
         given()
