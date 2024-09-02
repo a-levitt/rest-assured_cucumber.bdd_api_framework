40c3df0c01257d35c2b389ab5e81e13e5e744c9c
Artem Trifonov <artemis.levitt@gmail.com>
2024-09-02 21:23:01 +0200

    Support independent and sequential scenarios run with cucumber hooks

--- src/test/java/stepDefinitions/Hooks.java
+++ src/test/java/stepDefinitions/Hooks.java
@@ -10,8 +10,11 @@ public class Hooks {
     @Before("@DeletePlace")
     public void beforeScenario() throws IOException {
         AddPlaceStepDefinition stepDefinition = new AddPlaceStepDefinition();
-        stepDefinition.add_place_payload_with("HouseHold", "Montenegrin", "Podgorica, 13.Jula");
-        stepDefinition.user_calls_with_post_http_request("addPlaceAPI", "POST");
-        stepDefinition.verify_place_id_created_maps_to_using("HouseHold", "POST");
+
+        if (AddPlaceStepDefinition.place_id == null) {
+            stepDefinition.add_place_payload_with("HouseHold", "Montenegrin", "Podgorica, 13.Jula");
+            stepDefinition.user_calls_with_post_http_request("addPlaceAPI", "POST");
+            stepDefinition.verify_place_id_created_maps_to_using("HouseHold", "getPlaceAPI");
+        }
     }
 }
