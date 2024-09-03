d9fd9806a64ab78ec259a50d504ecb8c6d1c16e4
Artem Trifonov <artemis.levitt@gmail.com>
2024-09-03 13:27:05 +0200

    Add cucumber html-reports for tests

--- src/test/java/cucumber/Options/TestRunner.java
+++ src/test/java/cucumber/Options/TestRunner.java
@@ -5,6 +5,6 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/java/features", glue = {"stepDefinitions"}, tags = "@AddPlace or @DeletePlace")
+@CucumberOptions(features = "src/test/java/features", plugin = "json:target/jsonReports/cucumber-report.json", glue = {"stepDefinitions"}, tags = "@AddPlace or @DeletePlace")
 public class TestRunner {
 }
